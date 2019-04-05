35b1543d8dfb21657adfc775062e6e9375c2cb17
ASUS <ASUS@DESKTOP-KVT4ML0>
2019-04-05 12:42:45 +0800

    序列化，消息队列

--- src/main/java/org/weixin/weixin/domain/InMessage.java
+++ src/main/java/org/weixin/weixin/domain/InMessage.java
@@ -1,5 +1,7 @@
 package org.weixin.weixin.domain;
 
+import java.io.Serializable;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -9,12 +11,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @XmlAccessorType(XmlAccessType.FIELD) // jaxb从字段获取配置信息
 @XmlRootElement(name = "xml")	// jxab读取xml时根元素名称
-public class InMessage {
+public abstract class InMessage implements Serializable{
 	
 	// 由于微信发送给我们的消息字段是首字母大写的，而java字段字母大写不符合javaBean的规范
 	// 如果使用jaxb，需要使用@XmlElement注解xml里面元素名称和java字段之间的关系
 	// 如果使用Jackson，则需要使用@JackProperty注解指定java字段序列化和反序列化的字段名称映射
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@XmlElement(name = "ToUserName")
 	@JsonProperty("ToUserName")
 	private String toUserName;
--- src/main/java/org/weixin/weixin/domain/video/VideoInMessage.java
+++ src/main/java/org/weixin/weixin/domain/video/VideoInMessage.java
@@ -14,6 +14,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @XmlRootElement(name = "xml")
 public class VideoInMessage extends InMessage {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@XmlElement(name = "ThumbMediaId")
 	@JsonProperty("ThumbMediaId")
 	private String  ThumbMediaId;
